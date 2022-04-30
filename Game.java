5b6a0e5500c8f94915b37483f9e88378dc51b5b6
Will King <will@Wills-MacBook-Air-3.local>
2022-04-29 22:57:34 -0700

    Added compression and decompression

--- Game.java
+++ Game.java
@@ -1,9 +1,51 @@
 import java.io.*;
 
 public class Game {
-    
-    public Game() {
 
+    private char[][] map;
+    private String splitChar, splitLine;
+
+    public Game(char[][] map) {
+        this.map = map;
+        splitChar = " ";
+        splitLine = "|";
+    }
+
+    public void set(String comp) {
+        int x = 0;
+        int y = 0;
+
+        for(int i=0; i<comp.length(); i++) {
+            char c = comp.charAt(i);
+            if(c+"" == splitChar) continue;
+            if(c+"" == splitLine) {
+                y++;
+                x = 0;
+            } else {
+                map[x][y] = c;
+            }
+        }
+    }
+
+    public String compress() {
+        String s = "";
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[i].length; j++) {
+                s += map[i][j] + splitChar;
+            }
+            s += splitLine;
+        }
+        return s.substring(0, s.length()-2);
+    }
+
+    public void print() {
+        for(int i=0; i<map.length; i++) {
+            System.out.println();
+            for(int j=0; j<map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+        }
+        System.out.println();
     }
 
 }
